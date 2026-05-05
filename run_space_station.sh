dfb546767baa5ef8af4ed8ceb4a6df1df8b3be2a
rhit-fowlerlm <131782667+rhit-fowlerlm@users.noreply.github.com>
2026-05-04 21:34:46 -0400

    Updated paths

--- run_space_station.sh
+++ run_space_station.sh
@@ -2,6 +2,6 @@ if [ -z $XDG_SESSION_DESKTOP ]
 then
 echo "not in the desktop"
 else
-source /home/space-station/Documents/test/env/bin/activate
-python /home/space-station/Documents/test/main.py
+source /home/space-station/env/bin/activate
+python /home/space-station/main.py
 fi
