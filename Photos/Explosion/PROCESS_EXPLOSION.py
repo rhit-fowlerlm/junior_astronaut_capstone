7b7f577e8dd58214eff1cf7c40599085fc91a898
rhit-fowlerlm <131782667+rhit-fowlerlm@users.noreply.github.com>
2026-05-13 16:59:14 -0400

    Updated Animation Frames

--- Photos/Explosion/PROCESS_EXPLOSION.py
+++ Photos/Explosion/PROCESS_EXPLOSION.py
@@ -1,9 +1,23 @@
 from PIL import Image
 import os
+import numpy as np
 
 path = os.path.join("Photos", "Explosion")
 
 img = Image.open(os.path.join(path, "exp2_alpha_1.png"))
+
+img = img.convert("RGBA")
+datas = img.getdata()
+
+newData = []
+for item in datas:
+    d_white = int(np.sqrt((255 - item[0])**2 + (255 - item[1])**2 + (255 - item[2])**2))
+    if  d_white < 100:
+        newData.append((item[0], item[1], item[2], d_white))
+    else:
+        newData.append(item)
+
+img.putdata(newData)
 
 k = 15
 
