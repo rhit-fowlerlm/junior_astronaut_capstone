7b61935d1e6587b47a51b2569c15bafe87d64af5
rhit-fowlerlm <131782667+rhit-fowlerlm@users.noreply.github.com>
2026-05-14 20:12:05 -0400

    Updated write

---
+++ WriteArduinoFirmware/write_firmware.py
@@ -0,0 +1,29 @@
+from serial.tools.list_ports import comports
+import os
+
+firmware_file = "~/junior_astronaut_capstone/Arduino/.pio/build/megaatmega2560/firmware.hexls"
+
+def run():
+    ports = comports(include_links=True)
+    device = None
+    for p in ports:
+        if p.description is not None and "arduino" in p.description.lower():
+            device = p.device
+        if p.manufacturer is not None and "arduino" in p.manufacturer.lower():
+            device = p.device
+        if p.product is not None and "arduino" in p.product.lower():
+            device = p.device
+        if p.name is not None and "arduino" in p.name.lower():
+            device = p.device
+        if p.hwid is not None and "arduino" in p.hwid.lower():
+            device = p.device
+        if p.interface is not None and "arduino" in p.interface.lower():
+            device = p.device
+
+    if device is None:
+        return
+    
+    cmd = f"avrdude -c arduino -P {device} -b 115200 -p atmega2560 -D -U flash:w:{firmware_file}:i"
+
+
+run()
